ab8d5ff2eba9116f8461239c60abc9911ccf5121
Ubuntu <ubuntu@kubeflow-demo.lxd>
2020-08-27 17:27:00 +0000

    update create-rados-user script

--- infrastructure/kubeflow/create-rados-user.sh
+++ infrastructure/kubeflow/create-rados-user.sh
@@ -3,7 +3,7 @@ if [ -d "./generated" ]; then
 	mkdir generated
 fi
 
-juju ssh -m k8s ceph-mon/0 'sudo radosgw-admin user create --uid="ubuntu9" --display-name="Ubuntu9 Ceph"' | tee generated/rgw_user.txt
+juju ssh -m k8s ceph-mon/0 'sudo radosgw-admin user create --uid="ubuntu" --display-name="Ubuntu Ceph"' | tee generated/rgw_user.txt
 
 cat generated/rgw_user.txt | jq '.keys[0].access_key' | sed 's/\"//g' > generated/access_key.txt
 cat generated/rgw_user.txt | jq '.keys[0].secret_key' | sed 's/\"//g' > generated/secret_key.txt
