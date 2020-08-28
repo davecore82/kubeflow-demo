a10fe3c1cad9e41487d39baff64eab04985ab3d4
Ubuntu <ubuntu@kubeflow-demo.lxd>
2020-08-28 21:39:11 +0000

    fix tf-serving image and charm

--- infrastructure/kubeflow/deploy-tf-serving.sh
+++ infrastructure/kubeflow/deploy-tf-serving.sh
@@ -5,5 +5,5 @@ juju add-model serving kubeflow-k8s --config update-status-hook-interval=30s
 BUCKET_NAME=MYBUCKET
 S3_ENDPOINT=$(juju run -m k8s --unit ceph-radosgw/0 "unit-get private-address")
 
-juju deploy  cs:~kubeflow-charmers/tf-serving-10  --storage models=kubernetes,, --config model-name=tfserving --config model-base-path=s3://$BUCKET_NAME/tfserving --config aws-access-key-id=$(cat generated/access_key.txt) --config aws-region=us-east-1 --config aws-secret-access-key=$(cat generated/secret_key.txt) --config s3-endpoint=$S3_ENDPOINT --config s3-verify-ssl="0" --config s3-use-https="0" --resource oci-image=afrikha/tfserving-kafka
+juju deploy  cs:~kubeflow-charmers/tf-serving  --storage models=kubernetes,, --config model-name=tfserving --config model-base-path=s3://$BUCKET_NAME/tfserving --config aws-access-key-id=$(cat generated/access_key.txt) --config aws-region=us-east-1 --config aws-secret-access-key=$(cat generated/secret_key.txt) --config s3-endpoint=$S3_ENDPOINT --config s3-verify-ssl="0" --config s3-use-https="0" --resource oci-image=afrikha/tfserving-kafka  --config env-vars="KAFKA_IP=172.16.7.149"
 
--- infrastructure/kubeflow/tf-serving-kafka-k8s-image/source/entrypoint.sh
+++ infrastructure/kubeflow/tf-serving-kafka-k8s-image/source/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
-exec python /usr/bin/consumer.py
+exec python /usr/bin/consumer.py &
 
+tensorflow_model_server --port=9000 --rest_api_port=${TF_SERVING_SERVICE_PORT_TF_SERVING_REST} --model_name=${MODEL_NAME} --model_base_path=${MODEL_BASE_PATH} "$@"
